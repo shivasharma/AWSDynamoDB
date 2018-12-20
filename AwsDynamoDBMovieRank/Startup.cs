a8fd51b985c1eebe0a46c1110428d735a644618a
shiva sharma <shivathebravo@gmail.com>
2018-12-20 17:06:11 -0500

    Added Project

--- AwsDynamoDBMovieRank/Startup.cs
+++ AwsDynamoDBMovieRank/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,8 @@ namespace AwsDynamoDBMovieRank
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddAWSService<IAmazonDynamoDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +27,8 @@ namespace AwsDynamoDBMovieRank
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+            
         }
     }
 }
---
+++ MovieRank.Libs/Mappers/Mapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRank.Libs.Mappers
+{
+    class Mapper:IMapper
+    {
+    }
+}
